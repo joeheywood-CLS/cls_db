6635df14727245d42b67abc2b6e3abdee334fcdc
joe <joe@340895.vps-10.com>
2017-05-22 17:15:38 +0100

    reworking the ingest code. Better use of S4 classes

--- cls_class_basic.R
+++ cls_class_basic.R
@@ -13,8 +13,13 @@ setClass("ctg", representation(code = "numeric",
 setClass("cls_df", contains = "data.frame")
 
 setClass("cls_v", contains = "numeric", 
-         representation(vName = "character", label = "character"))
+         representation(vName = "character", label = "character",
+						ctg = "ctg"))
 
+
+setClass("cls_c", contains = "character", 
+         representation(vName = "character", label = "character",
+						ctg = "ctg"))
 ## cls_v_ctg
 setClass("cls_v_ctg", contains = "cls_v",
          representation(ctg = "ctg", values = "numeric"))
@@ -24,9 +29,39 @@ setClass("cls_v_num", contains = "cls_v",
          representation(ctg = "ctg", range = "numeric"))
 
 
-## cls_v_dbl
+## from a haven read_sav or read_dta column, get a cls_v object
+savToCLS_v <- function(n, df) {
+	## get ctg
+	ctg <- new("ctg")
+	x <- df[[n]]
+	att <- attributes(x)
+	if(!is.null(att$labels)) {
+		ctg@code <- as.numeric(att$labels)
+		ctg@label <- names(att$labels)
+		ctg@missing <- rep(FALSE, length(ctg@code))
+		if(!is.null(att$na_values)) {
+			ctg@missing[which(ctg@code %in% att$na_values)] <- TRUE
+		}
+	}
+	new("cls_v", as.numeric(df[[n]]), vName = n, label = att$label, ctg = ctg)
+}
 
-## cls_v_chr
+
+cls_toJSON <- function(x) {
+	out <- list()
+	cat(paste0(" ."))
+	out$ctg <- list(code = x@ctg@code, label = x@ctg@label,
+					   missing = x@ctg@missing)
+	out$vName <- x@vName
+	out$label <- x@label
+	jsonlite::toJSON(out)
+}
+
+cls_fromJSON <- function(j) {
+
+}
+
+
 
 
 ## cls_tab
--- rawToPrep.R
+++ rawToPrep.R
@@ -9,6 +9,8 @@ library(haven)
 library(jsonlite)
 library(readr)
 
+source("/home/joe/cls_db/cls_class_basic.R")
+f <- "/home/joe/mcs/raw/mcs5_cm_derived.sav"
 
 ################ Only "public" function here should be prepareSav #############
 
@@ -17,21 +19,30 @@ prepareSAV <- function(f, nm, prepHome = "/home/joe/mcs/dbprep/", drop = FALSE) 
     # create directory
     prepHome <- paste0(prepHome, nm)
     dir.create(prepHome)
-    st <- getMetaHaven(f)
+	## get data from file
+    st <- getDataFromSav(f)
     ## create schema ##
-    schm <- createSchemaFromList(st$meta, nm, prepHome, drop)
+    schm <- createSchemaFromCLS_df(st, nm, prepHome, drop)
     writeLines(schm, file.path(prepHome, paste0(nm, "_schema.sql")) )
     ## create meta table sql and insert json
-    mtSch <- createMetaSchema(st$meta, nm, drop)
+    mtSch <- createMetaSchema(st, nm, drop)
     writeLines(mtSch, file.path(prepHome, paste0(nm, "_m_schema.sql")) )
-    df <- st$dat
-    ## How to deal with missing values? ""? "."? "<NA>"
 	## write delimited file with | and no headers
-    write_delim(df, file.path(prepHome, paste0(nm, "_data.dat")), delim = "|", 
+    write_delim(st, file.path(prepHome, paste0(nm, "_data.dat")), delim = "|", 
 				na="", col_names = FALSE )
 }
 
 ###############################################################################
+
+getDataFromSav <- function(flnm) {
+	dat <-read_sav(flnm, user_na = TRUE)
+	cls_df <- list() 
+	for(cln in colnames(dat)) {
+		dat[[cln]][which(is.na(dat[[cln]]))] <- -42
+		cls_df[[cln]] <- savToCLS_v(cln, dat)
+	}
+	new("cls_df", as.data.frame(cls_df))
+}
 
 getMetaHaven <- function(flnm) {
     fl <- read_sav(flnm, user_na = TRUE)
@@ -42,66 +53,59 @@ getMetaHaven <- function(flnm) {
 }
 
 populateM <- function(nm, fl) {
-    cat(paste0("\n", nm, " " ))
-    if(grepl("^[fF]", attr(fl[[nm]], "format.spss")) == TRUE) {
-        cat(" NUMERIC ")
-        dbType <- getNumericType(fl[[nm]])
-        cat(" . ")
-    } else {
-        cat(" STRING ")
-        dbType <- getCharType(fl[[nm]])
-    }
-    cat(" . ")
+    x <- fl[[nm]]
     list(id = nm,
-         label = attr(fl[[nm]], "label"),
-         formatSpss = attr(fl[[nm]], "format.spss"),
-         dbType = dbType,
-         ctgry = elemsListM(fl[[nm]], nm)
+         label = attr(x, "label"),
+         formatSpss = attr(x, "format.spss"),
+         dbType = getDBType(x),
+         ctgry = elemsListM(x, nm)
  	)
 }
 
-getNumericType <- function(x) {
-    if(all(is.na(x))) {
+getDBType <- function(x) {
+	if(all(is.na(x))) {
         return("TINYINT")
     }
-    
-    x <- x[which(!is.na(x))]
-    if(all(floor(x) == x)) {
-        if(all(x %in% -127:127)) {
-            return("TINYINT")
-        } else if(all(x %in% -32767:32767)) {
-            return("SMALLINT")
-        } else{
-            return("INT")
-        }
-    } else {
-        return("DECIMAL")
-    }
+	if(is.numeric(x)) {
+		x <- x[which(!is.na(x))]
+		if(all(floor(x) == x)) {
+			if(all(x %in% -127:127)) {
+				return("TINYINT")
+			} else if(all(x %in% -32767:32767)) {
+				return("SMALLINT")
+			} else{
+				return("INT")
+			}
+		} else {
+			return("DECIMAL")
+		}
+	} else {
+		longest <- max(nchar(x))
+		if(length(unique(nchar(x))) == 1) {
+			return(sprintf("CHAR(%d)", longest))
+		}
+		if(longest < 200) {
+			return(sprintf("VARCHAR(%d)", longest))
+		} else {
+			return("TEXT")
+		}
+	}
 }
 
-getCharType <- function(x) {
-    longest <- max(nchar(x))
-    if(length(unique(nchar(x))) == 1) {
-        return(sprintf("CHAR(%d)", longest))
-    }
-    if(longest < 200) {
-        return(sprintf("VARCHAR(%d)", longest))
-    } else {
-        return("TEXT")
-    }
-}
 
-createSchemaFromList <- function(l, tblNm, flPth, drop = FALSE) {
-	first <- ifelse(drop == TRUE, paste0("DROP TABLE ", tblNm, ";"), "")
-    start <- sprintf("CREATE TABLE %s (", tblNm)
+createSchemaFromCLS_df <- function(l, tblNm, flPth, drop = FALSE) {
+	## Args: l - cls_df, tblNm - name of table, flPth - where .dat file is saved
+	##       drop - whether to drop table 
+	sql_drop <- ifelse(drop == TRUE, paste0("DROP TABLE ", tblNm, ";"), "")
+    sql_create <- sprintf("CREATE TABLE %s (", tblNm)
+    sql_joins <- vapply(l, function(itm) {
+        sprintf("%s %s", itm@vName, getDBType(itm))
+    }, "")
     end <- ");"
 	flPth <- paste0(flPth, "/", tblNm, "_data.dat")
-    last <- sprintf("COPY INTO %s FROM '%s';", tblNm, flPth)
-    ix <- 1
-    mid <- vapply(l, function(itm) {
-        sprintf("%s %s", itm$id, itm$dbType)
-    }, "")
-    c(first, start, paste(mid, collapse = ", \n"), end, last)
+    sql_copy <- sprintf("COPY INTO %s FROM '%s';", tblNm, flPth)
+	## Value: character vector of SQL command
+    c(sql_drop, sql_create, paste(sql_joins, collapse = ", "), end, sql_copy)
 }
 
 
@@ -109,12 +113,12 @@ createMetaSchema <- function(l, tblNm, drop = FALSE) {
 	first <- ifelse(drop == TRUE, paste0("DROP TABLE m_", tblNm, ";"), "")
     start <- sprintf("CREATE TABLE m_%s (", tblNm)
     mid <- vapply(l, function(itm) {
-        sprintf("%s text", itm$id)
+        sprintf("%s text", itm@vName)
     }, "")
     end <- ");"
     insSt <- sprintf("INSERT INTO m_%s VALUES (", tblNm)
     insts <- lapply(l, function(itm) {
-        jstr <- toJSON(itm)
+        jstr <- cls_toJSON(itm)
 		jstr <- gsub("'", " ", jstr)
 		sprintf("'%s'", jstr)
     })
